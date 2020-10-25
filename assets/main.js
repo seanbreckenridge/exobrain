3ae2c488afed37d396ce408507eb12c1addf8220
Sean Breckenridge <seanbrecke@gmail.com>
2020-10-25 03:51:59 -0700

    autoformat, fix CSS syntax errors

--- assets/main.js
+++ assets/main.js
@@ -1,72 +1,74 @@
 // Load from end of HTML without async to avoid putting
 // everything in document read handler.
 
-let D = document
-let body = D.querySelector('body')
-let sidebarButton = D.querySelector('.sidebar-button')
-let navLinks = D.querySelector('.nav-links')
-let bwtoggle = D.getElementById('bwtoggle')
-let subtitle = D.querySelector('h1+h2')
+let D = document;
+let body = D.querySelector("body");
+let sidebarButton = D.querySelector(".sidebar-button");
+let navLinks = D.querySelector(".nav-links");
+let bwtoggle = D.getElementById("bwtoggle");
+let subtitle = D.querySelector("h1+h2");
 
 // here so not rendered in Lynx
-let copied = D.createElement('div')
-copied.style.position = 'fixed'
-copied.style.top = '0'
-copied.style.right = '0'
-copied.style.textAlign = 'center'
-copied.style.borderRadius = '0.25rem'
-copied.style.zIndex = '200'
-copied.style.margin = '0.5rem'
-copied.style.padding = '0.25rem 1rem'
-copied.innerHTML = ""
-copied.style.background = 'black'
-copied.style.color = 'white'
+let copied = D.createElement("div");
+copied.style.position = "fixed";
+copied.style.top = "0";
+copied.style.right = "0";
+copied.style.textAlign = "center";
+copied.style.borderRadius = "0.25rem";
+copied.style.zIndex = "200";
+copied.style.margin = "0.5rem";
+copied.style.padding = "0.25rem 1rem";
+copied.innerHTML = "";
+copied.style.background = "black";
+copied.style.color = "white";
 //copied.style.width = '200%'
-copied.style.display = 'none'
-body.appendChild(copied)
+copied.style.display = "none";
+body.appendChild(copied);
 
 // D.onreadystatechange = e => {
 //  D.readyState === "complete" && location.hash && scrollBy(0,-20)
 // }
 
 // force *all* external links to open new tab
-D.querySelectorAll('a[href^="http"]')
-  .forEach(l=>l.setAttribute("target","_blank"))
+D.querySelectorAll('a[href^="http"]').forEach((l) =>
+  l.setAttribute("target", "_blank")
+);
 
 // progressively upgrade to full search if javascript
-D.querySelectorAll('a[href*="lite"]')
-  .forEach(l=>l.setAttribute('href',l.getAttribute('href').replace('lite','')))
+D.querySelectorAll('a[href*="lite"]').forEach((l) =>
+  l.setAttribute("href", l.getAttribute("href").replace("lite", ""))
+);
 
-const showCopied = e => {
-  copied.style.display = 'block'
-  copied.style.opacity = '0.85'
-  setTimeout(_=>{
-    copied.style.opacity = '0'
-    copied.style.display = 'none'
-  },1000)
-}
+const showCopied = (e) => {
+  copied.style.display = "block";
+  copied.style.opacity = "0.85";
+  setTimeout((_) => {
+    copied.style.opacity = "0";
+    copied.style.display = "none";
+  }, 1000);
+};
 
-addEventListener('click', e => {
-  let t = e.target
+addEventListener("click", (e) => {
+  let t = e.target;
 
-  if (t.nodeName==='CODE' && t.parentNode.nodeName === 'PRE') {
-    t.classList.toggle('zoomed')
-    copyToClipboard(t.innerText)
-    if (t.classList.contains('zoomed')) {
-      showCopied(e)
-      copied.innerHTML = "copied code snippet to clipboard"
+  if (t.nodeName === "CODE" && t.parentNode.nodeName === "PRE") {
+    t.classList.toggle("zoomed");
+    copyToClipboard(t.innerText);
+    if (t.classList.contains("zoomed")) {
+      showCopied(e);
+      copied.innerHTML = "copied code snippet to clipboard";
     }
-    return
+    return;
   }
 
   // if item is not a header or subtitle, exit
-  if (t.nodeName[0] !== 'H' || t === subtitle) return
+  if (t.nodeName[0] !== "H" || t === subtitle) return;
 
-  // copy link for 
-  let base = t.baseURI.split('#')[0]
-  let basea = base.split('/')
-  let node=basea[basea.length-2]
-  if (t.id === node) return
+  // copy link for
+  let base = t.baseURI.split("#")[0];
+  let basea = base.split("/");
+  let node = basea[basea.length - 2];
+  if (t.id === node) return;
 
   // TODO add better target validation
   // let link = `${base}#${t.id}`
@@ -74,46 +76,43 @@ addEventListener('click', e => {
   // copyToClipboard(link)
   // copied.innerHTML = "copied link to clipboard"
   // showCopied(e)
-})
+});
 
 // convert all heading tags to links
 D.querySelectorAll("h1,h2,h3,h4,h5,h6").forEach((h) => {
-
   // create URL
-  let base = h.baseURI.split('#')[0]
-  let basea = base.split('/')
-  let node=basea[basea.length-2]
-  if (h.id === node) return
-  let link = `${base}#${h.id}`
+  let base = h.baseURI.split("#")[0];
+  let basea = base.split("/");
+  let node = basea[basea.length - 2];
+  if (h.id === node) return;
+  let link = `${base}#${h.id}`;
 
   // create link element to replace into header
-  let linkel = D.createElement("a")
-  linkel.appendChild(D.createTextNode(h.innerHTML))
-  linkel.title = h.innerHTML
-  linkel.href = link
-  h.innerHTML = ""
-  h.appendChild(linkel)
-})
+  let linkel = D.createElement("a");
+  linkel.appendChild(D.createTextNode(h.innerHTML));
+  linkel.title = h.innerHTML;
+  linkel.href = link;
+  h.innerHTML = "";
+  h.appendChild(linkel);
+});
 
 // positions links to headers properly
 // onhashchange = e => scrollBy(0,-10)
 
-const copyToClipboard = str => {
-  const el = D.createElement('textarea')
+const copyToClipboard = (str) => {
+  const el = D.createElement("textarea");
   el.value = str;
-  el.setAttribute('readonly', '')
-  el.style.position = 'absolute'               
-  el.style.left = '-9999px'
-  D.body.appendChild(el)
-  const selected =            
-    D.getSelection().rangeCount > 0 
-      ? D.getSelection().getRangeAt(0)
-      : false
-  el.select()
-  D.execCommand('copy')
-  D.body.removeChild(el)
+  el.setAttribute("readonly", "");
+  el.style.position = "absolute";
+  el.style.left = "-9999px";
+  D.body.appendChild(el);
+  const selected =
+    D.getSelection().rangeCount > 0 ? D.getSelection().getRangeAt(0) : false;
+  el.select();
+  D.execCommand("copy");
+  D.body.removeChild(el);
   if (selected) {
-    D.getSelection().removeAllRanges()
-    D.getSelection().addRange(selected)
+    D.getSelection().removeAllRanges();
+    D.getSelection().addRange(selected);
   }
-}
+};
--- assets/search.js
+++ assets/search.js
@@ -1,46 +1,43 @@
 (() => {
-
   function createResultDiv(resultItem) {
-
     // from Fuse results
     let url = resultItem.item.url;
     let contents = resultItem.item.text;
 
     // entire result div
-    let resultDiv = document.createElement('div')
-    resultDiv.className = "searchresult-row"
+    let resultDiv = document.createElement("div");
+    resultDiv.className = "searchresult-row";
 
     // title
-    let resultTitle = document.createElement('span')
-    resultTitle.className = "searchresults-title"
+    let resultTitle = document.createElement("span");
+    resultTitle.className = "searchresults-title";
 
     // link for title
-    let resultLink = document.createElement("a")
+    let resultLink = document.createElement("a");
     resultLink.href = url;
-    resultLink.append(document.createTextNode(url))
+    resultLink.append(document.createTextNode(url));
 
     // contents
-    let resultContents = document.createElement("div")
-    resultContents.className = "searchresults-contents"
+    let resultContents = document.createElement("div");
+    resultContents.className = "searchresults-contents";
 
-    resultContents.append(document.createTextNode(contents.substring(0, 200)))
+    resultContents.append(document.createTextNode(contents.substring(0, 200)));
 
     // construct
-    resultTitle.append(resultLink)
-    resultDiv.append(resultTitle)
-    resultDiv.append(resultContents)
+    resultTitle.append(resultLink);
+    resultDiv.append(resultTitle);
+    resultDiv.append(resultContents);
 
     resultDiv.onmouseup = function (a) {
       if (a.ctrlKey || a.metaKey || a.button == 4) {
         a.preventDefault(); // prevent middle click from opening tab, so it doesnt open twice.
         window.open(url, "_blank"); // new tab
       } else {
-        window.location = url
+        window.location = url;
       }
+    };
 
-    }
-
-    return resultDiv
+    return resultDiv;
   }
 
   let fuseOptions = {
@@ -56,59 +53,56 @@
     // useExtendedSearch: false,
     ignoreLocation: false, // dont care about where this is in the search text
     // ignoreFieldNorm: false,
-    keys: [
-      "url",
-      "text"
-    ]
-  }
+    keys: ["url", "text"],
+  };
 
   // globla references
   let searchField = document.getElementById("search");
   let searchDiv = document.getElementById("searchresults");
 
   // initialize data
-  searchDiv.innerHTML = "Loading Search Index..."
+  searchDiv.innerHTML = "Loading Search Index...";
   let jsonData = null;
   let fuse = null;
 
   // send request to load index
   // use XHR for compability, since I'm not using babel
-  let xhr = new XMLHttpRequest()
-  xhr.open("GET", "/search_index.json")
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "/search_index.json");
   xhr.onreadystatechange = function () {
     // request complete
     if (xhr.readyState !== 4) return;
     if (xhr.status >= 400) {
-      searchDiv.innerHTML = "Couldn't load search index..."
-      console.error(xhr)
+      searchDiv.innerHTML = "Couldn't load search index...";
+      console.error(xhr);
     } else {
       // success!
-      jsonData = JSON.parse(this.responseText)
-      searchDiv.innerHTML = "..."
-      fuse = new Fuse(jsonData, fuseOptions)
+      jsonData = JSON.parse(this.responseText);
+      searchDiv.innerHTML = "...";
+      fuse = new Fuse(jsonData, fuseOptions);
     }
-  }
-  xhr.send()
+  };
+  xhr.send();
 
   // attach onchange function for input
   function updateResults() {
     if (fuse === null) {
-      searchDiv.innerHTML = 'Loading Search Index...' // make sure
-      return
+      searchDiv.innerHTML = "Loading Search Index..."; // make sure
+      return;
     }
     // clear previous results
-    searchDiv.innerHTML = '...'
-    results = fuse.search(searchField.value.toLowerCase())
+    searchDiv.innerHTML = "...";
+    results = fuse.search(searchField.value.toLowerCase());
     if (results.length == 0) {
-      searchDiv.innerHTML = 'Nothing found...'
+      searchDiv.innerHTML = "Nothing found...";
     } else {
-      searchDiv.innerHTML = results.length.toString() + ' results:'
+      searchDiv.innerHTML = results.length.toString() + " results:";
       for (let i = 0; i < results.length; i++) {
-        searchDiv.append(createResultDiv(results[i]))
+        searchDiv.append(createResultDiv(results[i]));
       }
     }
   }
 
-  searchField.addEventListener('input', updateResults);
-  searchField.addEventListener('propertychange', updateResults); // for IE8
-})()
+  searchField.addEventListener("input", updateResults);
+  searchField.addEventListener("propertychange", updateResults); // for IE8
+})();
