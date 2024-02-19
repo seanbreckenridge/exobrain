fa4e13080d3dac7b9368128db1603d79e2b37a58
Sean Breckenridge <seanbrecke@gmail.com>
2024-02-18 17:08:28 -0800

    fix rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
-      link: `/blog/${post.slug}/`,
+      link: `${import.meta.env.BASE_URL}blog/${post.slug}/`,
     })),
   });
 }
