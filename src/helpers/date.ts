46602691e4d05ccf5fcf365ff360a3f5b2efaf7a
Sean Breckenridge <seanbrecke@gmail.com>
2024-07-24 14:04:04 -0700

    simplify journal date

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -1,19 +1,13 @@
-function journalDate(date: Date) {
+export function journalDate(date: Date) {
+  // https://billwurtz.com/notebook.html esque
   // this formats the date like:
   // 1.1.21  12:00 am
-  const day = date.getDate();
   const month = date.getMonth() + 1; // Months are zero-indexed, so we add 1
   const year = date.getFullYear() % 100; // Extract last two digits of the year
 
   const hours = date.getHours();
-  const minutes = date.getMinutes();
+  const minutes = date.getMinutes().toString().padStart(2, "0");
   const ampm = hours >= 12 ? "pm" : "am";
 
-  const formattedDate = `${month}.${day}.${year}  ${hours % 12 || 12}:${
-    minutes < 10 ? "0" : ""
-  }${minutes} ${ampm}`;
-
-  return formattedDate;
+  return `${month}.${date.getDay()}.${year}  ${hours % 12 || 12}:${minutes} ${ampm}`;
 }
-
-export { journalDate };
